1b96edbcc44a5dae6b35ce977068a111f5df4446
Bruno Monteiro <bruno.mm1992@gmail.com>
2026-06-01 23:22:51 -0300

    fix assets path for github pages

--- src/core/assets.ts
+++ src/core/assets.ts
@@ -1,5 +1,5 @@
 /** Path of an asset served from public/assets/. */
-export const assetUrl = (path: string) => `/assets/${path}`;
+export const assetUrl = (path: string) => `assets/${path}`;
 
 /** Path of a rasterized frame of a sprite from FFDec. */
 export const spriteFrame = (defineSpriteDir: string, frame = 1) =>
